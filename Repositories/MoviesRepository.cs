ee39ddf6d57bd216950a10b064f473c1fa9ed9ba
ysantonyance <zinovevakarina569@gmail.com>
2026-09-11 20:38:58 +0300

    Updated info for repository

--- Repositories/MoviesRepository.cs
+++ Repositories/MoviesRepository.cs
@@ -63,8 +63,106 @@ namespace MoviesBackend.Repository
         {
             return new List<Movie>
             {
-                new Movie { Id = 1, Title = "Inception", Type = "Movie", Director = "Christopher Nolan", Genre = "Sci-Fi", ReleaseYear = 2010, Img = "https://image.tmdb.org/t/p/w500/edv5CZvWj09upOsy2Y6IwDhK8bt.jpg", Description = "A thief who steals corporate secrets..." },
-                new Movie { Id = 2, Title = "Interstellar", Type = "Movie", Director = "Christopher Nolan", Genre = "Sci-Fi", ReleaseYear = 2014, Img = "https://image.tmdb.org/t/p/w500/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg", Description = "A team of explorers travel through a wormhole..." }
+                new Movie {
+                    Id = 1,
+                    Title = "Backrooms",
+                    Type = "Movie",
+                    Director = "David F. Sandberg",
+                    Genre = "Psychological Horror, Psychological Thriller, Supernatural Horror, Horror, Sci-Fi, Thriller",
+                    ReleaseYear = 2026,
+                    Img = "/images/Backrooms.jpg",
+                    Description = "After a therapist's patient disappears into a dimension beyond reality, she must venture into the unknown to save him."
+                },
+                new Movie { 
+                    Id = 2,
+                    Title = "Girl from Nowhere",
+                    Type = "TV Series",
+                    Director = "Jatuphong Rungrueangdechaphat, S Khomkrit Treewimol, Pairach Khumwan, Sitisiri Mongkolsiri, Varayu Ruksku, Siwawut Sewatanon, Chaianan Soijumpa, Apiwat Supateerapong, T-Thawat Taifayongvichit, Pokpong Pairach Khumwan, Paween Purijitpanya, Surawut Tungkharak",
+                    Genre = "Thai, Psychological Drama, Teen Horror, Crime, Drama, Fantasy, Horror, Mystery, Thriller",
+                    ReleaseYear = 2018,
+                    Img = "/images/GirlFromNowhere.jpg",
+                    Description = "A mysterious, clever girl named Nanno transfers to different schools, exposing the lies and misdeeds of the students and faculty at every turn."
+                },
+                new Movie {
+                    Id = 3,
+                    Title = "A Minecraft Movie",
+                    Type = "Movie",
+                    Director = "Jared Hess",
+                    Genre = "High-Concept Comedy, Quest, Slapstick, Survival, Action, Adventure, Comedy, Family, Fantasy",
+                    ReleaseYear = 2025,
+                    Img = "/images/AMinecraftMovie.jpg",
+                    Description = "Four misfits are suddenly pulled through a mysterious portal into a bizarre cubic wonderland that thrives on imagination. To get back home they'll have to master this world while embarking on a quest with an unexpected expert crafter."
+                },
+                new Movie {
+                    Id = 4,
+                    Title = "Arcane",
+                    Type = "TV Series",
+                    Director = "Arnaud Delord, Pascal Charrue, Bart Maunoury, Marietta Ren, Christelle Abgrall, Etienne Mattera",
+                    Genre = "Action Epic, Adult Animation, Computer Animation, Dark Fantasy, Dystopian Sci-Fi, Epic, Fantasy, Political Drama, Psychological Drama, Steampunk",
+                    ReleaseYear = 2021,
+                    Img = "/images/Arcane.jpg",
+                    Description = "Amid the stark discord of twin cities Piltover and Zaun, two sisters fight on rival sides of a war between magic technologies and clashing convictions."
+                },
+                new Movie {
+                    Id = 5,
+                    Title = "Michael",
+                    Type = "Movie",
+                    Director = "Antoine Fuqua",
+                    Genre = "Coming-of-Age, Docudrama, Period Drama, Psychological Drama, Drama, Biography, History, Music",
+                    ReleaseYear = 2026,
+                    Img = "/images/Michael.jpg",
+                    Description = "The early life of musician Michael Jackson, from the discovery of his talent as the lead of the Jackson Five to the artist whose creative ambition fueled a pursuit to become the biggest entertainer in the world."
+                },
+                new Movie {
+                    Id = 6,
+                    Title = "Obsession",
+                    Type = "Movie",
+                    Director = "Curry Barker",
+                    Genre = "Dark Romance, Psychological Horror, Psychological Thriller, Supernatural Horror, Horror, Romance, Thriller",
+                    ReleaseYear = 2025,
+                    Img = "/images/Obsession.jpg",
+                    Description = "After breaking the mysterious \"One Wish Willow\" to win his crush's heart, a hopeless romantic finds himself getting exactly what he asked for but soon discovers that some desires come at a dark, sinister price."
+                },
+                new Movie {
+                    Id = 7,
+                    Title = "Avatar: The Last Airbender",
+                    Type = "TV Series",
+                    Director = "Craig Mazin, Neil Druckmann, Kantemir Balagov, Ali Abbasi, Peter Hoar, Jasmila Žbanić, Lisa Cholodenko, Jeremy Webb",
+                    Genre = "Avatar: The Last Airbender, Adventure Epic, Epic, Fantasy Epic, Globetrotting Adventure, Hand-Drawn Animation, Martial Arts, Quest, Superhero, Supernatural Fantasy",
+                    ReleaseYear = 2005,
+                    Img = "/images/AvatarTheLastAirbender.jpg",
+                    Description = "After a century frozen in ice, young Avatar Aang journeys with friends to master the four elements and bring balance to a world at war with the Fire Nation."
+                },
+                new Movie {
+                    Id = 8,
+                    Title = "Dexter",
+                    Type = "Tv Series",
+                    Director = "James Manos Jr.",
+                    Genre = "Dark Comedy, Psychological Drama, Psychological Thriller, Serial Killer, Suspense Mystery Whodunnit, Crime, Dtama, Mystery, Thriller",
+                    ReleaseYear = 2006,
+                    Img = "/images/Dexter.jpg",
+                    Description = "He's smart. He's lovable. He's Dexter Morgan, America's favorite serial killer, who spends his days solving crimes and his nights committing them.\r\n\r\n"
+                },
+                new Movie {
+                    Id = 9,
+                    Title = "Black Swan",
+                    Type = "Movie",
+                    Director = "Darren Aronofsky",
+                    Genre = "Epic, Psychological Drama, Psychological Thriller, Showbiz Drama, Drama, Thriller",
+                    ReleaseYear = 2010,
+                    Img = "/images/BlackSwan.jpg",
+                    Description = "An insecure ballerina finally gets the role she has always dreamed of, but as the pressures mount her line between reality and illusion starts to blur.\r\n\r\n"
+                },
+                new Movie {
+                    Id = 10,
+                    Title = "Spirited Away",
+                    Type = "Movie",
+                    Director = "Hayao Miyazaki",
+                    Genre = "Japanese, Anime, Coming-of-Age, Fairy Tale, Hand-Drawn Animation, Isekai, Supernatural Fantasy, Adventure, Animation, Family",
+                    ReleaseYear = 2001,
+                    Img = "/images/SpiritedAway.jpg",
+                    Description = "During her family's move to the suburbs, a sullen 10-year-old girl wanders into a world ruled by gods, witches and spirits, and where humans are changed into beasts.\r\n\r\n"
+                },
             };
         }
 
